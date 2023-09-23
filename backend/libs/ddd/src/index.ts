b6908af30de0d846bd48cf45dc3a18fc75d8aa65
thangnv-0703 <sunshines0703@gmail.com>
2023-09-23 16:13:06 +0700

    update common library and ddd library for backend

---
+++ backend/libs/ddd/src/index.ts
@@ -0,0 +1,7 @@
+export * from './lib/aggregate-root.base';
+export * from './lib/command.base';
+export * from './lib/domain-event.base';
+export * from './lib/entity.base';
+export * from './lib/mapper.interface';
+export * from './lib/query.base';
+export * from './lib/value-object.base';
