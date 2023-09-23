d24c9c73e4c76110db4827d4d1e2baf84b14c0d4
thangnv-0703 <sunshines0703@gmail.com>
2023-09-23 18:28:27 +0700

    rename service name and add sample service

--- backend/apps/post-service/jest.config.ts
+++ backend/apps/post-service/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'post',
+  displayName: 'post-service',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/post',
+  coverageDirectory: '../../coverage/apps/post-service',
 };
--- backend/apps/sample-service-e2e/jest.config.ts
+++ backend/apps/sample-service-e2e/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'notification-e2e',
+  displayName: 'sample-service-e2e',
   preset: '../../jest.preset.js',
   globalSetup: '<rootDir>/src/support/global-setup.ts',
   globalTeardown: '<rootDir>/src/support/global-teardown.ts',
@@ -15,5 +15,5 @@ export default {
     ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/notification-e2e',
+  coverageDirectory: '../../coverage/sample-service-e2e',
 };
