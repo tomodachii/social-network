2cc039a97b632d55daa4bb46a113497c7e2fad72
thangnv-0703 <sunshines0703@gmail.com>
2023-10-01 14:34:29 +0700

    feat: update file main of sample service

--- backend/apps/sample-service/src/main.ts
+++ backend/apps/sample-service/src/main.ts
@@ -5,7 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+``;
 import { AppModule } from './app.module';
 import { MikroORM } from '@mikro-orm/core';
 
